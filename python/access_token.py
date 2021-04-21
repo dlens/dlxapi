bf68afe2c25955e7adf0e8fbe4554f6554047c5a
Bill Adams <wjadams@decisionlens.com>
2021-04-21 14:39:58 -0400

    Update access_token.py

--- python/access_token.py
+++ python/access_token.py
@@ -1,12 +1,13 @@
 import requests
 
-from constants import base_url, client_id, client_secret
+from constants import base_url
 from from_json import FromJson
 
 
-def get_access_token():
-    grant_type = 'client_credentials'
-    auth_url = base_url + "/oauth/token?grant_type=client_credentials"
+def get_access_token(client_id, client_secret, api_instance_url=None, grant_type='client_credentials'):
+    if api_instance_url is None:
+        api_instance_url = base_url
+    auth_url = api_instance_url + "/oauth/token?grant_type=client_credentials"
 
     response = requests.post(auth_url, auth=(client_id, client_secret),
                             data={"grant_type": grant_type, "client_id": client_id, "client_secret": client_secret})
